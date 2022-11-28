1d6206a57ab66c38686ecb60195eb70a79ef341a
Walker Riley <riley.855@osu.edu>
2022-11-28 16:15:09 -0500

    translator implemented & .txt file can be read from

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 from googletrans import Translator   # need to pip install googletrans==3.1.0a0^C
 
-translator = Translator()
+
+def open_file(file_name):
+    translator = Translator()
+    with open(file_name, 'r', encoding='utf8') as file:
+        for line in file:
+            print(translator.translate(text=line, dest="en").text)
+
+
+open_file("vietExample.txt")
