e4b89049698505115b633d9fb7859e2881e1a826
kennytru19 <112277525+kennytru19@users.noreply.github.com>
2022-11-30 13:25:15 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from googletrans import Translator   # need to pip install googletrans==3.1.0a0^C
+import threading
 
+#List that holds the resulting documents translated to English
+global translatedToEnglish
 
 def open_file(file_name):
     translator = Translator()
@@ -7,5 +10,25 @@ def open_file(file_name):
         for line in file:
             print(translator.translate(text=line, dest="en").text)
 
+def translatingFunction(threadName):
+    translatedToEnglish.append()
+    # I think we should translate the files within this threading function
+
+    #Pseudocode:
+    # Read each line
+    # Translate each line
+    # Write each line to a new text document? or string? idk
+    # Loop again 
+    # Lock (I think the function is acquire())
+    # translatedToEnglish.append(the final text dcoument)
+    # Unlock (I think it's release())
+
+# She said something about translating multiple documents -- maybe we can use a list of documents ie. documents[0] = vietExample.txt, documents[1] = chineseExample.txt
+# and then iterate through each document and pass each document to the threading function?
+documents = []
+
+for document in documents:
+    thread = threading.Thread(target = translatingFunction, args=document)
+    thread.start()
 
 open_file("vietExample.txt")
