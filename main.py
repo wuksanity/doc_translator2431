cd9036ec56979ca30b0d848cedc42b19bd461811
Walker Riley <riley.855@osu.edu>
2022-12-01 14:34:18 -0500

    organized functions and added input, need to figure out threads still

--- main.py
+++ main.py
@@ -1,34 +1,41 @@
 from googletrans import Translator   # need to pip install googletrans==3.1.0a0^C
 import threading
 
-#List that holds the resulting documents translated to English
+# List that holds the resulting documents translated to English
 global translatedToEnglish
+documents = []
 
-def open_file(file_name):
+
+def get_user_docs():
+    fmt = "file1, file2, ..."
+    docs = input(f"Enter the names of each document to translate in the following format: {fmt}")
+    documents_array = docs.split(", ")  # probably should validate this...
+    return documents_array
+
+
+def translating_function(thread_name, file_name):
     translator = Translator()
     with open(file_name, 'r', encoding='utf8') as file:
         for line in file:
             print(translator.translate(text=line, dest="en").text)
+    #translatedToEnglish.append()
 
-def translatingFunction(threadName):
-    translatedToEnglish.append()
-    # I think we should translate the files within this threading function
 
-    #Pseudocode:
+def begin_threading(docs):
+    for doc in docs:
+        thread = threading.Thread(target=translating_function, args=doc)
+        thread.start()
+
+
+documents = get_user_docs()
+print(documents)
+begin_threading(documents)
+
+# Pseudocode:
     # Read each line
     # Translate each line
     # Write each line to a new text document? or string? idk
-    # Loop again 
+    # Loop again
     # Lock (I think the function is acquire())
-    # translatedToEnglish.append(the final text dcoument)
+    # translatedToEnglish.append(the final text document)
     # Unlock (I think it's release())
-
-# She said something about translating multiple documents -- maybe we can use a list of documents ie. documents[0] = vietExample.txt, documents[1] = chineseExample.txt
-# and then iterate through each document and pass each document to the threading function?
-documents = []
-
-for document in documents:
-    thread = threading.Thread(target = translatingFunction, args=document)
-    thread.start()
-
-open_file("vietExample.txt")
