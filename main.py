dd9c67cf42d877f5667dfefc011a3d5fb51dbe71
Walker Riley <riley.855@osu.edu>
2022-12-05 22:56:34 -0500

    replaced locking with tuple manipulation, implemented file verification w/user

--- main.py
+++ main.py
@@ -7,37 +7,52 @@ documents = []
 def get_user_docs():
     fmt = "file1, file2, ..."
     docs = input(f"Enter the names of each document to translate in the following format: {fmt}")
-    documents_array = docs.split(", ")  # probably should validate this...
+    documents_array = docs.split(", ")
     return documents_array
+
+
+def check_user_files(documents):
+    documents_updated = documents
+    response = "n"
+    while response.lower() != "y":  # ensuring user's files are correct
+        print()
+        for idx, doc in enumerate(documents_updated):
+            print(f"{idx + 1}. {doc}")
+        print()
+        response = input("Do the above files look correct? (Y/N/y/n): ")
+        if response.lower() != "y":
+            print("Renewing file data...")
+            documents_updated = get_user_docs()
+        else:
+            response = "y"
+    return documents_updated
 
 
 def get_out_file(file_name):
     return file_name[0: file_name.index(".")] + ".output.txt"
 
 
-def translating_function(file_name, lock):
+def translating_function(files):
     translator = Translator()
-    output_file = get_out_file(file_name)
-    lock.acquire()
-    with open(file_name, 'r', encoding='utf8') as file:
-        with open(output_file, 'w') as out:
+    #lock.acquire()
+    with open(files[0], 'r', encoding='utf8') as file:
+        with open(files[1], 'w') as out:
             for line in file:
                 out.write(translator.translate(text=line, dest="en").text + "\n")
-
-    print()
-    print(file_name + f" has finished translating, view {output_file} to see!")
-    lock.release()
+    print(files[0] + f" has finished translating, view {files[1]} to see!")
+    #lock.release()
 
 
 def begin_threading(docs):
-    lock = threading.Lock()
-    for doc in docs:
-        thread = threading.Thread(target=translating_function, args=(*[doc], lock))
+    #lock = threading.Lock()
+    for doc_pair in docs:
+        thread = threading.Thread(target=translating_function, args=([doc_pair]))
         thread.start()
 
 
 documents = get_user_docs()
-print(documents)
+documents = check_user_files(documents)
+documents = list(zip(documents, map(get_out_file, documents)))  # creating tuple: (in_file, out_file)
 begin_threading(documents)
 
 # vietExample.txt, mandarinExample.txt
